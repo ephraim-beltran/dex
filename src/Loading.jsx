7723c4ef925e9762f006bd85c11aed47bea07d2c
Ephraim Beltran <github@beltran.cc>
2023-03-25 05:41:16 +0000

    Created a placeholder loading component

---
+++ src/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return ( 
+        <div className="loading-container">
+            <div className="loading">Loading...</div>
+        </div>
+     );
+}
+ 
+export default Loading;
