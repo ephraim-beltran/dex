afcd73d22fcc288f08b06c3d38e3c8e6ae184d7e
Ephraim Beltran <github@beltran.cc>
2023-04-07 09:22:11 +0000

    Code optimization

--- src/TypeCalculator/fetchWeakness.js
+++ src/TypeCalculator/fetchWeakness.js
@@ -15,74 +15,87 @@ const fetchTypeData = async (typeId, controller) => {
             }
         }
     `;
-
-  const response = await fetch("https://beta.pokeapi.co/graphql/v1beta", {
+  const url = "https://beta.pokeapi.co/graphql/v1beta";
+  const response = await fetch(url , {
     signal: controller.signal,
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ query }),
   });
 
-  if (!response.ok) {
-    throw new Error(
-      `Type data not received. Responded with ${response.status}`
-    );
-  }
-
-  const data = await response.json();
-  return data;
+  return response;
 };
 
 const fetchWeakness = (typeList) => {
   const [weakness, setWeakness] = useState([]);
-  const [error, setError] = useState();
+  const [sortedWeakness, setSortedWeakness] = useState([]);
   
   useEffect(() => {
     const controller = new AbortController();
     
     const getList = async () => {
-      const list = await Promise.all(typeList.map(type => fetchTypeData(type.id, controller)))
-      
-      const datas = await Promise.all(list.map(
-        ({
-          data: {
-            pokemon_v2_type_by_pk: {
-              pokemonV2TypeefficaciesByTargetTypeId: weaknesses
-            }
-          }
-        }) => {
-        return weaknesses
-      }));
-
-      let filteredList = [];
-      datas.forEach(data => {
-        data.forEach(item => {
-          const index = filteredList.findIndex(obj => obj.damage_type === item.pokemon_v2_type.name);
-          if (index !== -1) {
-            filteredList[index].damage_factor = filteredList[index].damage_factor * item.damage_factor / 100
-          } else {
-            filteredList.push({
-              damage_factor: item.damage_factor,
-              damage_type: item.pokemon_v2_type.name
+        try {
+            const responses = await Promise.all(typeList.map(type => fetchTypeData(type.id, controller)))
+            responses.forEach(response => {
+                if (!response.ok) {
+                    throw new Error(
+                        `Pokemon type data not received. Responded with ${responses.status}`
+                    );
+                }
             })
-          }
-        })
-      })
-      const sortedList = filteredList.sort((a, b) => {
-        return b.damage_factor - a.damage_factor
-      })
-      setWeakness(filteredList);
 
-      // For debugging only
-      console.log('Weakness loaded for: ' +
-        typeList.map(type => type.name)
-      );
-    }
+            const data = await Promise.all(responses.map(response => response.json()))
 
-    getList().catch(err => {
-        if (err.name !== 'AbortError'){
-            setError(err.message)
+            return data;
+            
+        } catch (err) {
+            if (err.name !== 'AbortError'){
+                console.error(err.message)
+            }
         }
+    }
+    
+    getList()
+    .then(data => {
+        const weaknesses = data.map(({
+            data: {
+              pokemon_v2_type_by_pk: {
+                pokemonV2TypeefficaciesByTargetTypeId: typeWeaknesses
+              }
+            }
+          }) => {
+            return typeWeaknesses
+          }
+        )
+
+        let filteredList = []; // List that needs to be returned
+        // Pushes each array item and consolidates existing array item
+        weaknesses.forEach(weakness => {
+          weakness.forEach(item => {
+            const index = filteredList.findIndex(obj => obj.damage_type === item.pokemon_v2_type.name);
+
+            if (index !== -1) {
+              filteredList[index].damage_factor = filteredList[index].damage_factor * item.damage_factor / 100
+            } else {
+              filteredList.push({
+                damage_factor: item.damage_factor,
+                damage_type: item.pokemon_v2_type.name
+              })
+            }
+          })
+        })
+        // Sorts the filteredList in order of damage_factor
+        // Not necessary
+        const sortedList = filteredList.sort((a, b) => {
+          return b.damage_factor - a.damage_factor
+        })
+        setWeakness(filteredList);
+        setSortedWeakness(sortedList);
+
+        // For debugging only
+        console.log('Weakness loaded for: ' +
+          typeList.map(type => type.name)
+        );
     })
 
     return () => controller.abort();
@@ -90,6 +103,7 @@ const fetchWeakness = (typeList) => {
 
   return {
     weakness,
+    sortedWeakness
   };
 };
 
