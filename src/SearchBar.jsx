ec20f1481c2facf5083a58e2251bc80e5fe6e0d0
Ephraim Beltran <github@beltran.cc>
2023-04-17 06:41:37 +0000

    Modified search logic

--- src/SearchBar.jsx
+++ src/SearchBar.jsx
@@ -17,17 +17,19 @@ const SearchBar = ({ setResults, children: resultList }) => {
 
   const getList = (e) => {
     setSearchInput(e.target.value);
-    const list = data.pokemon_v2_pokemonspecies;
-    const filteredList = list.filter((pokemon) => {
-      const pokemonName = pokemon.name.toLowerCase();
-      const input = e.target.value.toLowerCase();
-      return pokemonName.includes(input);
-    });
+    const list = data;
+    if (error !== null) {
+      setResults([{name: 'Cannot fetch list', id: 0}])
+      return console.log(error)
+    };
     if (e.target.value.length > 2) {
+      const filteredList = list.filter((pokemon) => {
+        const pokemonName = pokemon.name.toLowerCase();
+        const input = e.target.value.toLowerCase();
+        return pokemonName.includes(input);
+      });
       setResults(filteredList);
     }
-    if (e.target.value.length < 3) setResults([]);
-    if (error !== null) console.log(error);
   };
 
   const clearInput = (e) => {
