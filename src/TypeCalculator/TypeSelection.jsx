e08c7791f2a887b7191ebc1bc1e0fb830c7ed6c9
Ephraim Beltran <github@beltran.cc>
2023-04-17 08:11:03 +0000

    Created a TypeCalculator test page

---
+++ src/TypeCalculator/TypeSelection.jsx
@@ -0,0 +1,14 @@
+import "./TypeCalculator.css";
+
+const TypeSelection = () => {
+  return (
+    <section>
+      <div className="type-selector">
+        <input type="checkbox" />
+        <div className="slider round">normal</div>
+      </div>
+    </section>
+  );
+};
+
+export default TypeSelection;
